32c55f924dbbe31731d51f20f7ddae928122084a
soju6jan <cybersol@naver.com>
2019-12-03 16:13:11 +0900

    1

--- worker_start.sh
+++ worker_start.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 export C_FORCE_ROOT='true'
 ps -ef | grep sjva.celery | awk '{print $1}' | xargs kill -9
-python -OO ./venv/bin/celery worker -A sjva.celery --loglevel=info
+#python -OO ./venv/bin/celery worker -A sjva.celery --loglevel=info
+python -OO /usr/local/bin/celery worker -A sjva.celery --loglevel=info
