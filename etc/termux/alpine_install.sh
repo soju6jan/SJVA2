ef332dc8c26c818bf38cc8635e43c6fedd09ba56
soju6jan <cybersol@naver.com>
2019-12-13 20:03:02 +0000

    1

--- etc/termux/alpine_install.sh
+++ etc/termux/alpine_install.sh
@@ -7,7 +7,7 @@ apk add --no-cache python2 python2-dev py-pip
 apk add py2-lxml py2-gevent py2-pillow py2-crypto py2-sqlalchemy py2-markupsafe py2-mako py2-httplib2 py2-google-api-python-client py2-psutil py2-scandir
 apk add --no-cache ffmpeg libxslt-dev 
 apk add --no-cache --virtual .build-deps  gcc g++ make libffi-dev openssl-dev linux-headers libxml2-dev jpeg-dev zlib-dev 
-pip install -r /home/SJVA2/etc/termux_requirements.list
+pip install -r /home/SJVA2/etc/termux/termux_requirements.list
 apk del .build-deps
 apk --update add tzdata
 cp /usr/share/zoneinfo/Asia/Seoul /etc/localtime
