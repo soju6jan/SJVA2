e378cd2a5a6648b1b049df18a23b0d4a41d33513
soju6jan <cybersol@naver.com>
2019-12-21 16:03:43 +0000

    1

--- etc/termux/sjva_install.sh
+++ etc/termux/sjva_install.sh
@@ -3,7 +3,8 @@ cd $HOME
 curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/termux_bash_profile
 mv termux_bash_profile ~/.profile
 
-curl -LO https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh
+#curl -LO https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh
+curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/TermuxAlpine.sh
 bash TermuxAlpine.sh
 
 curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/alpine_install.sh
