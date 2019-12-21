c67cbf4cbd60ddc44869c214b315a9d107f70617
soju6jan <cybersol@naver.com>
2019-12-21 17:42:30 +0000

    1

--- etc/termux/sjva_install.sh
+++ etc/termux/sjva_install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 cd $HOME
 cd ../usr/share
-pkg -y install proot
+pkg install -y proot
 curl -LO https://soju6jan.com/file/TermuxAlpine.tar.gz
 tar -zxvf TermuxAlpine.tar.gz
 rm TermuxAlpine.tar.gz
 mv TermuxAlpine/startalpine ../bin
 curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/my_start.sh
 mv my_start.sh ./TermuxAlpine/home/SJVA2/
+chmod +x ./TermuxAlpine/home/SJVA2/my_start.sh
 curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/sjva.db_
 mv sjva.db_ ./TermuxAlpine/home/SJVA2/data/db/
 cd $HOME
 curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/termux_bash_profile
 mv termux_bash_profile ~/.profile
-echo 'SJVA copy completed.. Rerun Termux'
-exit
+echo 'SJVA copy completed.. Re-run Termux.'
+bash exit
