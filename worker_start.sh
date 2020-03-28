0a4d9ace8bb4b7265f66f7814573ae4f2df3b83d
soju6jan <cybersol@naver.com>
2020-03-28 16:05:15 +0900

    1

--- worker_start.sh
+++ worker_start.sh
@@ -6,7 +6,8 @@ if [ -f "export.sh" ] ; then
     echo "Run export.sh end"
 fi
 export C_FORCE_ROOT='true'
-ps -ef | grep sjva.celery | awk '{print $1}' | xargs kill -9
+#ps -ef | grep sjva.celery | awk '{print $1}' | xargs kill -9
+ps -eo pid,args | grep sjva.celery | grep -v grep | awk '{print $1}' | xargs -r kill -9
 #python -OO ./venv/bin/celery worker -A sjva.celery --loglevel=info
 #nohup python -OO /usr/local/bin/celery -A sjva.celery flower --port=${CELERY_MONOTORING_PORT} &
 nohup python -OO /usr/local/bin/celery worker -A sjva.celery -c${CELERY_WORKER_COUNT} --loglevel=info &
